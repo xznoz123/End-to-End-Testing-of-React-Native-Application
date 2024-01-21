00d675ec097cc950cf2bc910457c521a83cb7494
xznoz123 <ashish.789sahay@gmail.com>
2024-01-22 00:47:09 +0530

    ashish

---
+++ app/test/maestro.config.js
@@ -0,0 +1,11 @@
+// maestro.config.js
+
+module.exports = {
+    scenarios: [
+      {
+        file: 'React Native\Nativefolder\AwesomeProject\app\.maestro\Watchlater.yaml', // Adjust the path accordingly
+      },
+      // Add more scenarios as needed
+    ],
+  };
+  
